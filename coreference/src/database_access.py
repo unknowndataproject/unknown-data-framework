030fa7530198cdd2686228c93fa6e90799354fe9
Lu Gan <ganlu@spkogpu01.gesis.intra>
2025-07-14 20:52:50 +0200

    update gesis database reading

--- coreference/src/database_access.py
+++ coreference/src/database_access.py
@@ -32,10 +32,42 @@ def read_gesis_dataset_json_gz(filepath, encoding='utf-8'):
         data = json.loads(fin.read().decode(encoding))
     
     data = data['hits']['hits']
-    # print(data[0].keys())
-    # print(data[0]['_source'].keys())
-    data = [{'_id': record['_id']} | {k:v for k,v in record['_source'].items() if k in ['doi','title_info', 'locations', 'content_info']} for record in data]
-    # print(data[0])
-    return data
+    pprint(data[0])
+    print(data[0]['_source'].keys())
+    data = [{'_id': record['_id']} | {k:v for k,v in record['_source'].items() if k in ['doi','title_info', 'locations', 'collection_info','content_info', 'date']} for record in data]
+    print(data[0]['date'])
+    mapped_data = []
+    for e in data:
+        _e = {}
+        for k,v in e.items():
+            try:
+                if k == 'title_info':
+                    _e["dataset_name"] = v["title"]
+                elif k == "content_info":
+                    if "content" in v: 
+                        _e["description"] = v["content"]
+                    elif "content_de" in v:
+                        _e["description"] = v["content_de"]
+                elif k == "doi":
+                    _e["homepage"] = "https:/doi.org/" + v
+                elif k == "date":
+                    if "pub_date" in v:
+                        _e["introduced_date"] = v["pub_date"]
+                    else:
+                        _e["introduced_date"] = None
+                    _v = {kk:vv for kk,vv in v.items() if kk != "pub_date"}
+                    if len(_v) > 0:
+                        _e['date'] = _v
+                elif k == "collection_info":
+                    if "collection" in v:
+                        _e["collection"] = v["collection"]
+                    else:
+                        _e["collection"] = v["collection_de"]
+            except KeyError as e:
+                print(v)
+                raise e
+        mapped_data.append(_e)
+
+    return mapped_data
 
 # read_dataset_metadata_from_file('./data/datasets.json.gz')
